2ccca2fe40256d8c2f14a0634d90524ab64435e9
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-20 19:53:11 -0500

    moving to test different areas

--- Longest-Collatz-Sequence.py
+++ Longest-Collatz-Sequence.py
@@ -2,9 +2,12 @@ checkNum = 0
 longestNum = 0
 counter = 1
 
-for x in range(9, 1000000, 2):
+for x in range(400001, 500000, 2):
     checkNum = x
     while(checkNum > 1):
+        #if(checkNum == 427630):
+            #counter += 124
+            #break;
         if(checkNum % 2 == 0):
             checkNum = (checkNum / 2)
         else:
@@ -12,6 +15,7 @@ for x in range(9, 1000000, 2):
         counter += 1
     if(counter > longestNum):
         longestNum = x
+        print longestNum
     counter = 1
     
 print longestNum
