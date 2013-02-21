8225cb0ee89e7ab7a0c071f8440bd2176bbb9436
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-20 20:07:26 -0700

    Fixed logical issue - works quickly

--- Longest-Collatz-Sequence.py
+++ Longest-Collatz-Sequence.py
@@ -1,21 +1,19 @@
 checkNum = 0
 longestNum = 0
+longestCounter = 0
 counter = 1
 
-for x in range(400001, 500000, 2):
-    checkNum = x
-    while(checkNum > 1):
-        #if(checkNum == 427630):
-            #counter += 124
-            #break;
-        if(checkNum % 2 == 0):
-            checkNum = (checkNum / 2)
-        else:
-            checkNum = ((checkNum * 3) + 1)
-        counter += 1
-    if(counter > longestNum):
-        longestNum = x
-        print longestNum
-    counter = 1
+for x in range(3, 1000001):
+	checkNum = x
+	while(checkNum > 1):
+		if(checkNum % 2 == 0):
+			checkNum = (checkNum / 2)
+		else:
+			checkNum = ((checkNum * 3) + 1)
+		counter += 1
+	if(counter > longestCounter):
+		longestNum = x
+		longestCounter = counter
+	counter = 1
     
 print longestNum
